4c57aee7512fcac0c2736de3043c7b22f6d9f260
Itanor Strapazzon <itanor@gmail.com>
2017-11-13 22:57:59 -0200

    switch

---
+++ src/hello/switch.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+  "fmt"
+  "time"
+)
+
+func main() {
+  i := 2
+  fmt.Print("Write ", i, " as ")
+
+  switch i {
+    case 1:
+      fmt.Println("one")
+    case 2:
+      fmt.Println("two")
+    case 3:
+      fmt.Println("three")
+  }
+
+  switch time.Now().Weekday() {
+    case time.Saturday, time.Sunday:
+      fmt.Println("It's the weekend")
+    default:
+      fmt.Println("It's a weekday")
+  }
+
+  whatAmI := func(i interface{}) {
+    switch t := i.(type) {
+      case bool:
+        fmt.Println("I'm a bool")
+      case int:
+        fmt.Println("I'm an int")
+      default:
+        fmt.Printf("Don't know type %T\n", t)
+    }
+  }
+  whatAmI(true)
+  whatAmI(1)
+  whatAmI("hey")
+}
+
